24f98ee2e5ac3c2cc258f7f181aa258d05aec8bd
Kayley <kayley.luftig@gmail.com>
2019-04-10 15:04:57 +0000

    Adds looping exercises

---
+++ chorus-for.js
@@ -0,0 +1,8 @@
+//for loop
+var chorus = "Let's dance!";
+//similar to while loop, but consendsed on the same line
+for (var repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+
+console.log("Until the sun comes up!");
---
+++ chorus-while.js
@@ -0,0 +1,12 @@
+// while loop exercise
+var chorus = "Lets dance!";
+var repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log ("*change key*");
+  }
+    console.log(chorus);
+    // repeat++ is shorthand for repeat = repeat + 1
+    repeat++;
+}
+console.log("Until the sun comes up!");
